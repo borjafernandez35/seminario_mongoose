bcc36c50ec1cf9ed23f9f54407fd26b41ed21498
borjafernandez35 <borjaf29@gmail.com>
2024-10-08 11:06:43 +0200

    CRUD

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -1,11 +1,18 @@
-import { Schema, model, connect } from 'mongoose';
+import mongoose, { Schema, model, connect, ObjectId } from 'mongoose';
 
 
 // 1. Create an interface representing a document in MongoDB.
 interface IUser {
+  _id?: mongoose.Types.ObjectId;
   name: string;
   email: string;
   avatar?: string;
+}
+
+interface IProduct{
+  product: string;
+  property:mongoose.Types.ObjectId;
+  price: String
 }
 
 // 2. Create a Schema corresponding to the document interface.
@@ -14,9 +21,15 @@ const userSchema = new Schema<IUser>({
   email: { type: String},
   avatar: String
 });
-
+const productSchema= new Schema<IProduct>({
+  product: { type: String },
+  property:{type: Schema.Types.ObjectId,required: true, ref: 'User'},
+  price: String
+})
 // 3. Create a Model.
 const User = model('User', userSchema);
+
+const Product = model('Product', productSchema);
 
 
 run().catch(err => console.log(err));
@@ -25,11 +38,23 @@ async function run() {
   // 4. Connect to MongoDB
   await connect('mongodb://127.0.0.1:27017/test');
 
-  const user1:  IUser = {
-    "name": 'Bill',
-    "email": 'bill@initech.com',
-    "avatar": 'https://i.imgur.com/dM7Thhn.png'
-  };
+  const user1= new User({
+    name: 'Bill',
+    email: 'bill@initech.com',
+    avatar: 'https://i.imgur.com/dM7Thhn.png'
+  });
+
+   await user1.save(); 
+
+  const product1= new Product({
+    product: 'Coke',
+    price: '1€',
+    property: user1._id
+  });
+
+  await product1.save();
+
+
   
   const user2:  IUser = {
     "name": 'Billy',
@@ -37,11 +62,15 @@ async function run() {
     //"avatar": 'https://i.imgur.com/dM7Thhn.png'
   };
 
+  
+
   const user3:  IUser = {
     "name": 'Billion',
     "email": 'billic@initech.com',
     //"avatar": 'https://i.imgur.com/dM7Thhn.png'
   };
+
+  const newUser2= new User(user3); 
 
   const createUser = async () => {
     const user4 =  new User({
@@ -61,38 +90,46 @@ async function run() {
   }
   createUser();
 
-  async function getUser() {
-    const user = await User.findOne({name: 'Amparo'});
-    console.log(user)
- }
- getUser();
+  const getUser = async () => {
+    const user = await User.findOne({ name: 'Amparo' });
+    console.log('Found User:', user);
+  };
+  await getUser();
 
- async function updateUser() {
-  const user = await User.updateMany({name: 'Bill'}, {
-      email:'bill1@initech.com'
-  });
-  //console.log(user)
-}
-updateUser();
+ const updateUser = async () => {
+  const result = await User.updateOne(
+    { name: 'Billion' },
+    { email: 'Javier@initech.com' }
+  );
+  console.log('Update Result:', result);
+};
+await updateUser();
+
 
 const deleteUser = async () => {
-  const result = await User.deleteMany({name: 'Billy'});
-  console.log(result)
+  const result = await User.findOneAndDelete({name: 'Amparo'});
+  console.log('Delete Result:', result);
 }
 
+deleteUser();
 
-  const newUser= new User(user1);
+
+  
   const newUser1= new User(user2);
-  const newUser2= new User(user3);
-  await newUser.save();
+   
+   
   await newUser1.save();
   await newUser2.save();
-
-  console.log(newUser.email); // 'bill@initech.com'
+  
+  
+  console.log(user1.email); // 'bill@initech.com'
   console.log(newUser1.name);
   console.log(newUser2.name);
+  const productWithUser = await Product.findOne({ product: 'Coke' }).populate('property');
+  console.log('el producto es:',productWithUser);
+  
 
-  deleteUser();
+  
 
   
 
